0d1d16b204e5ef33b122b9952eb1f190ecbc2cfd
Isaias Gonçalves <ss.initech@gmail.com>
2021-04-29 10:29:39 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:app06_agenda_contatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Agenda com SQLite',
+      debugShowCheckedModeBanner: false,
+      home: HomePage() ,
+    ),
+  );
+  
+}
